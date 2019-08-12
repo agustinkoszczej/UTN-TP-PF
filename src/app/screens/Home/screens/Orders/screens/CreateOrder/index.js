bd2b87f040f90321940b3a3d994bceed0651f59d
Santiago Nucera <santinucera4@gmail.com>
2019-08-12 01:15:29 -0300

    Added dropdown

--- src/app/screens/Home/screens/Orders/screens/CreateOrder/index.js
+++ src/app/screens/Home/screens/Orders/screens/CreateOrder/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+import CustomDropdown from '@components/CustomDropdown';
+import { PAYMENT_METHODS } from '@constants/paymentMethods';
 
 class CreateOrderContainer extends Component {
+  state = { selectedOption: PAYMENT_METHODS[0].text };
+
+  handleSelectItem = item => this.setState({ selectedOption: item.text });
+
+  nameSelector = item => item.text;
+
   render() {
-    return <Text>Hola</Text>;
+    const { selectedOption } = this.state;
+    return (
+      <View>
+        <CustomDropdown
+          selectedOption={selectedOption}
+          onSelectItem={this.handleSelectItem}
+          items={PAYMENT_METHODS}
+          itemNameSelector={this.nameSelector}
+          closeOnOverlayPress
+        />
+      </View>
+    );
   }
 }
 
---
+++ src/constants/paymentMethods.js
@@ -0,0 +1,10 @@
+export const PAYMENT_METHODS = [
+  {
+    id: 'cash',
+    text: 'Efectivo'
+  },
+  {
+    id: 'mercadoPago',
+    text: 'Mercado Pago'
+  }
+];

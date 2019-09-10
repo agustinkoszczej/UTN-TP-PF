24c180a7ec7f726b32c892baeb4b08cba3691c2f
Santiago Nucera <santinucera4@gmail.com>
2019-09-10 20:22:46 -0300

    Fix modals

--- src/app/screens/Home/constants.js
+++ src/app/screens/Home/constants.js
@@ -1,7 +1,7 @@
 export const strings = {
   orderCreated: 'Pedido creado exitosamente',
   auctionCreated: 'Subasta creada exitosamente',
-  orderCreatedMessage: 'Puedes visualizar el pedido y su esta en la vista de Pedidos Activos',
-  auctionsCreatedMessage: 'Puedes visualizar la subasta y su esta en la vista de Subastas Activos',
+  orderCreatedMessage: 'Puedes visualizar el pedido en la vista de Pedidos Activos',
+  auctionsCreatedMessage: 'Puedes visualizar la subasta en la vista de Subastas Activas',
   continue: 'Continuar'
 };

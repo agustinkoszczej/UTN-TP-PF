b1bc8600769d73daba100a2328f774887bff25cf
agustinkoszczej <agustinkoszczej@gmail.com>
2019-10-28 19:35:13 -0300

    Fix texts

--- src/utils/pushNotificationUtils.js
+++ src/utils/pushNotificationUtils.js
@@ -6,7 +6,7 @@ export const pushMessage = notification => {
   return {
     [NOTIFICATIONS.NOTIFICATION_NEW_BID]: 'Nueva oferta recibida',
     [NOTIFICATIONS.NOTIFICATION_ORDER_CREATED]: 'Pedido creado',
-    [NOTIFICATIONS.NOTIFICATION_REQUEST_CONTACT]: 'Nueva solictud de conexión',
+    [NOTIFICATIONS.NOTIFICATION_REQUEST_CONTACT]: 'Nueva solictud de contacto',
     [NOTIFICATIONS.NOTIFICATION_ORDER_UPDATED]: 'Pedido actualizado',
     [NOTIFICATIONS.NOTIFICATION_NEW_MESSAGE]: `Nuevo mensaje de ${sender}`,
     [NOTIFICATIONS.NOTIFICATION_EXPIRED_AUCTION]: 'Subasta expirada'
